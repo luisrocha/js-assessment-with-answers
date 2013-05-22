ff958366f2845c731f41bdbfca0b7f70ca6b289e
Luis Rocha <luis88rocha@gmail.com>
2013-05-22 16:43:46 +0100

    solutions for Arrays, Numbers, Objects and Best Practices

--- app/arrays.js
+++ app/arrays.js
@@ -4,50 +4,131 @@ define(function() {
   return {
     indexOf : function(arr, item) {
 
+        return arr.indexOf(item);
     },
 
     sum : function(arr) {
 
+        for(var i = arr.length, array_sum = 0; i--; array_sum += arr[i]);
+
+        return array_sum;
     },
 
     remove : function(arr, item) {
 
+        var 
+        new_arr = [],
+        arr_length = arr.length;
+
+        for(var i = -1; ++i < arr_length;) {
+            if( arr[i] !== item ) {
+                new_arr.push(arr[i]);
+            }
+        }
+
+        return new_arr;
     },
     
     removeWithoutCopy : function(arr, item) {
 
+        var i = 0;
+        var arr_length = arr.length;
+
+        while( i < arr_length ) {
+            if( arr[i] === item ) {
+                arr.splice(i,1);
+                arr_length--;
+            }
+            else {
+                i++;
+            }
+        }
+
+        return arr;
     },
 
     append : function(arr, item) {
 
+        arr.push(item);
+        return arr;
     },
 
     truncate : function(arr) {
 
+        arr.pop();
+        return arr;
     },
 
     concat : function(arr1, arr2) {
 
+        return arr1.concat(arr2);
     },
 
     insert : function(arr, item, index) {
 
+        arr.splice(index, 0, item);
+        return arr;
     },
 
     count : function(arr, item) {
 
+        var counter = 0;
+
+        for( var i = arr.length; i--; ) {
+            if( arr[i] === item ) {
+                counter++;
+            }
+        }
+
+        return counter;
     },
 
     duplicates : function(arr) {
 
+        var
+        arr_length = arr.length,
+        dup_arr = [],
+        dup_arr_last_index = -1;
+
+        arr.sort();
+
+        for( var i = 0, last_num = arr[0]; i++ < arr_length; ) {
+
+            if( arr[i] === last_num && arr[i] !== dup_arr[dup_arr_last_index] ) {
+                dup_arr.push(arr[i]);
+                dup_arr_last_index++;
+            }
+
+            last_num = arr[i];
+        }
+
+        return dup_arr;
     },
 
     square : function(arr) {
 
+        var
+        arr_length = arr.length,
+        square_arr = [];
+
+        arr.forEach( function(num) {
+            square_arr.push(num*num);
+        });
+
+        return square_arr;
     },
 
     findAllOccurrences : function(arr, target) {
 
+        var occurences = [];
+
+        for( var i = arr.length; i--; ) {
+            if( arr[i] == target ) {
+                occurences.push(i);
+            }
+        }
+
+        return occurences;
     }
   };
 });
--- app/numbers.js
+++ app/numbers.js
@@ -4,18 +4,32 @@ define(function() {
   return {
     valueAtBit: function(num, bit) {
 
+        var bin_string = (num >> bit-1).toString(2);
+
+        return parseInt(bin_string[bin_string.length-1],10);
     },
 
     base10: function(str) {
 
+        return parseInt(str,2);
     },
 
     convertToBinary: function(num) {
 
+        var bin_string = num.toString(2);
+
+        while( bin_string.length < 8 ) bin_string = "0" + bin_string;
+
+        return bin_string;
     },
 
     multiply: function(a, b) {
 
+        var 
+        a_dp = Math.pow(10,String(a).length - String(a).indexOf('.') - 1), 
+        b_dp = Math.pow(10,String(b).length - String(b).indexOf('.') - 1); 
+
+        return (a * a_dp) * (b * b_dp) / (a_dp * b_dp);
     }
   };
 });
